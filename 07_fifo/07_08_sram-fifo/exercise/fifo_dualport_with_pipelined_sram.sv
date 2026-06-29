7423506723c5c76d4d78d1174400dd7973635120
SergeRuff <sershov9193@gmail.com>
2026-06-29 22:35:15 +0300

    WIP: Added pointers logic.

--- 07_fifo/07_08_sram-fifo/exercise/fifo_dualport_with_pipelined_sram.sv
+++ 07_fifo/07_08_sram-fifo/exercise/fifo_dualport_with_pipelined_sram.sv
@@ -13,6 +13,17 @@ module fifo_dualport_with_pipelined_sram #(
 );
 
     localparam int LATENCY = 5;
+    localparam int POINTER_WIDTH = $clog2(DEPTH);
+    localparam int COUNTER_WIDTH = $clog2(DEPTH+1);
+    localparam int MAX_PTR = POINTER_WIDTH'(DEPTH-1);
+
+    logic sram_wen;
+    logic sram_ren;
+    
+    logic wr_ptr;
+    logic wr_ptr_reg;
+    logic rd_ptr;
+    logic rd_ptr_reg;
 
     sram_dualport_latency_5 #(
         .WIDTH ( WIDTH ),
@@ -20,8 +31,8 @@ module fifo_dualport_with_pipelined_sram #(
     ) i_mem (
         .clk_i   (clk_i),
         .rst_i   (rst_i),
-        .wen_i   (),
-        .ren_i   (),
+        .wen_i   (sram_wen),
+        .ren_i   (sram_ren),
         .waddr_i (),
         .raddr_i (),
         .data_i  (data_i),
@@ -66,19 +77,23 @@ module fifo_dualport_with_pipelined_sram #(
     end :   wen_logic
 
     always_comb begin   :   write_pointer_logic
-
+                        wr_ptr = wr_ptr_reg;
+        if (sram_wen)   wr_ptr = (wr_ptr==MAX_PTR)? '0 : wr_ptr + 1;
     end :   write_pointer_logic
 
-    always_comb begin   :   write_pointer_register
-
+    always_ff @(posedge clk_i)  begin   :   write_pointer_register
+        if (rst_i)  wr_ptr_reg <= '0;
+        else        wr_ptr_reg <= wr_ptr;
     end :   write_pointer_register
 
     always_comb begin   :   read_pointer_logic
-
+                        rd_ptr = rd_ptr_reg;
+        if (sram_ren)   rd_ptr = (rd_ptr==MAX_PTR)? '0 : rd_ptr + 1;
     end :   read_pointer_logic
 
-    always_comb begin   :   read_pointer_register
-
+    always_ff @(posedge clk_i)  begin   :   read_pointer_register
+        if (rst_i)  rd_ptr_reg <= '0;
+        else        rd_ptr_reg <= rd_ptr;
     end :   read_pointer_register
 
     always_comb begin   :   empty_flag_logic
